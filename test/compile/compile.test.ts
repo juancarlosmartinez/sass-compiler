41cd8db973c19738d9fa437cff242950047f4dc7
Juan C. Martínez <juan88carlos@gmail.com>
2024-12-23 16:46:49 +0100

    Más tests.

--- test/compile/compile.test.ts
+++ test/compile/compile.test.ts
@@ -1,38 +1,110 @@
 import {Compiler} from "../../src/compile/compile";
 import {exists} from "../../src/util/fs";
-import {unlink} from "node:fs/promises";
+import {unlink, rm, writeFile, mkdir} from "node:fs/promises";
+import {Log} from "../../src/util/log";
+
+const scssContent = `
+    body {
+      .my-class {
+        color: #000000;
+      }
+    }
+`;
 
 describe('Compiler', () => {
+
+    beforeEach(async () => {
+        Log.init({
+            verbose: true
+        });
+        await mkdir('test/compile/scss', {
+            recursive: true
+        });
+        await writeFile('test/compile/scss/test.scss', scssContent);
+    });
 
     it('should build a new instance of Compiler', async () => {
         const compiler = Compiler.build();
 
         await compiler.compile({
             entries: [{
-                baseDir: 'test/compile',
-                outputDir: 'test/compile/css',
+                baseDir: 'test/compile/scss',
+                outputDir: 'test/compile/css/v1',
                 filenames: /\.scss$/
             }]
         });
 
         expect(compiler).toBeInstanceOf(Compiler);
 
-        expect(await exists('test/compile/css')).toBe(true);
-        expect(await exists('test/compile/css/test.css')).toBe(true);
+        expect(await exists('test/compile/css/v1')).toBe(true);
+        expect(await exists('test/compile/css/v1/test.css')).toBe(true);
 
-        await unlink('test/compile/test.scss');
+        await unlink('test/compile/scss/test.scss');
 
-        expect(await exists('test/compile/test.scss')).toBe(false);
-        expect(await exists('test/compile/css/test.css')).toBe(true);
+        expect(await exists('test/compile/scss//test.scss')).toBe(false);
+        expect(await exists('test/compile/css/v1/test.css')).toBe(true);
 
         await compiler.compile({
             entries: [{
-                baseDir: 'test/compile',
-                outputDir: 'test/compile/css',
+                baseDir: 'test/compile/scss',
+                outputDir: 'test/compile/css/v1',
                 filenames: /\.scss$/
             }]
         });
 
-        expect(await exists('test/compile/css/test.css')).toBe(false);
+        expect(await exists('test/compile/css/v1/test.css')).toBe(false);
+
+        await compiler.stop();
+    });
+
+    it('should build a new instance of Compiler with watch option', async () => {
+        const compiler = Compiler.build({
+            watch: true
+        });
+
+        expect(compiler).toBeInstanceOf(Compiler);
+
+        await compiler.compile({
+            entries: [{
+                baseDir: 'test/compile/scss',
+                outputDir: 'test/compile/css/v1',
+                filenames: /\.scss$/
+            }]
+        });
+
+        expect(await exists('test/compile/css/v1')).toBe(true);
+        expect(await exists('test/compile/css/v1/test.css')).toBe(true);
+
+        await unlink('test/compile/scss/test.scss');
+
+        expect(await exists('test/compile/scss/test.scss')).toBe(false);
+        expect(await exists('test/compile/css/v1/test.css')).toBe(true);
+
+        await compiler.stop();
+    });
+
+    // it('should not exists baseDir', async () => {
+    //     const compiler = Compiler.build();
+    //
+    //     await compiler.compile({
+    //         entries: [{
+    //             baseDir: 'test/non-existent',
+    //             outputDir: 'test/compile/css/v1',
+    //             filenames: /\.scss$/
+    //         }]
+    //     });
+    //
+    //     expect(compiler).toBeInstanceOf(Compiler);
+    //
+    //     await rm('test/compile/test.scss');
+    //
+    //     await compiler.stop();
+    // });
+
+    afterEach(async () => {
+        await rm('test/compile/css', {
+            recursive: true,
+            force: true,
+        });
     });
 });
