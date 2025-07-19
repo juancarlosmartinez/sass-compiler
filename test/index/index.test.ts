f6cf1fbe13c30f0cc9446855c0428d745d15cb98
Juan Carlos Martínez Expósito <juan88carlos@gmail.com>
2025-07-20 00:21:12 +0200

    Fix tests.

--- test/index/index.test.ts
+++ test/index/index.test.ts
@@ -1,4 +1,22 @@
+import {mkdir, rm, writeFile} from "node:fs/promises";
+
+const scssContent = `
+    body {
+      .my-class {
+        color: #000000;
+      }
+    }
+`;
+
 describe('Main Compiler', () => {
+
+    beforeEach(async () => {
+        await mkdir('test/index/scss', {
+            recursive: true
+        });
+        await writeFile('test/index/scss/test.scss', scssContent);
+    })
+
     it('should init compiler', async () => {
         const originalArgv = process.argv;
         process.argv = [
@@ -13,5 +31,18 @@ describe('Main Compiler', () => {
         await new Promise(resolve => setTimeout(resolve, 1000));
 
         process.argv = originalArgv
+    });
+
+    afterEach(async () => {
+        await Promise.all([
+            rm('test/index/scss', {
+                recursive: true,
+                force: true
+            }),
+            rm('test/index/css', {
+                recursive: true,
+                force: true
+            })
+        ]);
     });
 });
